2e9d462a59c68b0f8bae918728893ef03a113326
houssamedine <60809576+houssamedine@users.noreply.github.com>
2023-01-22 13:12:59 +0000

    finaliser module messager

--- src/private/messagers/messagers.controller.ts
+++ src/private/messagers/messagers.controller.ts
@@ -7,16 +7,27 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
+  Delete, HttpCode,
 } from '@nestjs/common';
 import { MessagersService } from './messagers.service';
 import { CreateMessagerDto } from './dto/create-messager.dto';
 import { UpdateMessagerDto } from './dto/update-messager.dto';
+import {DeleteMany} from "../../communs/generiques/delete_many";
 
 @ApiTags('Messagers')
 @Controller('messagers')
 export class MessagersController {
   constructor(private readonly messagersService: MessagersService) {}
+
+  //Add Multi Messages
+  @Post('bulk')
+  createBulk(@Body() createMessagerDto: CreateMessagerDto[]) {
+    try {
+      return this.messagersService.createBulk(createMessagerDto);
+    } catch (error) {
+      return new HttpException('', 404);
+    }
+  }
 
   //Add One Message
   @Post()
@@ -42,7 +53,7 @@ export class MessagersController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     try {
-      return this.messagersService.findOne(+id);
+      return this.messagersService.findOne(id);
     } catch (error) {
       return new HttpException('', 404);
     }
@@ -55,7 +66,17 @@ export class MessagersController {
     @Body() updateMessagerDto: UpdateMessagerDto,
   ) {
     try {
-      return this.messagersService.update(+id, updateMessagerDto);
+      return this.messagersService.update(id, updateMessagerDto);
+    } catch (error) {
+      return new HttpException('', 404);
+    }
+  }
+
+  //Delete Multi Messages
+  @Delete(':bulk')
+  removeBulk(@Body() deleted: DeleteMany) {
+    try {
+      return this.messagersService.removeBulk(deleted);
     } catch (error) {
       return new HttpException('', 404);
     }
@@ -65,7 +86,29 @@ export class MessagersController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     try {
-      return this.messagersService.remove(+id);
+      return this.messagersService.remove(id);
+    } catch (error) {
+      return new HttpException('', 404);
+    }
+  }
+
+  //Recover Multi Messages
+  @Post(':recover/bulk')
+  @HttpCode(200)
+  recoverBulk(@Body() deleted: DeleteMany) {
+    try {
+      return this.messagersService.recoverBulk(deleted);
+    } catch (error) {
+      return new HttpException('', 404);
+    }
+  }
+
+  //Recover One Message
+  @Post(':recover/:id')
+  @HttpCode(200)
+  recover(@Param('id') id: string) {
+    try {
+      return this.messagersService.recover(id);
     } catch (error) {
       return new HttpException('', 404);
     }
--- src/private/messagers/messagers.service.ts
+++ src/private/messagers/messagers.service.ts
@@ -1,51 +1,116 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateMessagerDto } from './dto/create-messager.dto';
-import { UpdateMessagerDto } from './dto/update-messager.dto';
+import {Messager} from './entities/messager.entity';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm';
+import {CreateMessagerDto} from './dto/create-messager.dto';
+import {UpdateMessagerDto} from './dto/update-messager.dto';
+import {In, Repository} from 'typeorm';
+import {InternalServerErrorException} from "@nestjs/common/exceptions";
+import {DeleteMany} from "../../communs/generiques/delete_many";
 
 @Injectable()
 export class MessagersService {
-  //Add One Message
-  create(createMessagerDto: CreateMessagerDto) {
-    try {
-      return 'This action adds a new messager';
-    } catch (error) {
-      throw new NotFoundException();
+    constructor(
+        @InjectRepository(Messager)
+        private readonly repositoryMessager: Repository<Messager>,
+    ) {
     }
-  }
 
-  //Find All Messages
-  findAll() {
-    try {
-      return 'This action adds a new messager';
-    } catch (error) {
-      throw new NotFoundException();
+    //Add Multi Messages
+    async createBulk(createMessagerDto: CreateMessagerDto[]): Promise<Messager[]> {
+        try {
+            return await this.repositoryMessager.save(createMessagerDto);
+        } catch (error) {
+            throw new NotFoundException();
+        }
     }
-  }
 
-  //Find One Message
-  findOne(id: number) {
-    try {
-      return 'This action adds a new messager';
-    } catch (error) {
-      throw new NotFoundException();
+    //Add One Message
+    async create(createMessagerDto: CreateMessagerDto): Promise<Messager> {
+        try {
+            return await this.repositoryMessager.save(createMessagerDto);
+        } catch (error) {
+            throw new NotFoundException();
+        }
     }
-  }
 
-  //Update One Message
-  update(id: number, updateMessagerDto: UpdateMessagerDto) {
-    try {
-      return 'This action adds a new messager';
-    } catch (error) {
-      throw new NotFoundException();
+    //Find All Messages
+    async findAll(): Promise<Messager[]> {
+        try {
+            return await this.repositoryMessager.find();
+        } catch (error) {
+            throw new NotFoundException();
+        }
     }
-  }
 
-  //Delete One Message
-  remove(id: number) {
-    try {
-      return 'This action adds a new messager';
-    } catch (error) {
-      throw new NotFoundException();
+    //Find One Message
+    async findOne(id: string): Promise<Messager> {
+        try {
+            return await this.repositoryMessager.findOne({where: {id}});
+        } catch (error) {
+            throw new NotFoundException();
+        }
     }
-  }
+
+    //Update One Message
+    async update(id: string, updateMessagerDto: UpdateMessagerDto): Promise<Messager> {
+        try {
+            await this.findOne(id);
+            const result = await this.repositoryMessager.update(id, updateMessagerDto);
+            if (result.affected > 0) return this.findOne(id);
+            else throw new InternalServerErrorException();
+        } catch (error) {
+            throw new NotFoundException();
+        }
+    }
+
+    //Delete Multi Messages
+    async removeBulk(deleted: DeleteMany): Promise<boolean> {
+        try {
+            const messageId = await this.repositoryMessager.find({where: {id: In(deleted.ids)}});
+            await this.repositoryMessager.softRemove(messageId);
+            return messageId.length > 0 ? true : false;
+        } catch (error) {
+            throw new NotFoundException();
+        }
+    }
+
+    //Delete One Message
+    async remove(id: string): Promise<boolean> {
+        try {
+            const messageId = await this.findOne(id);
+            await this.repositoryMessager.softRemove(messageId);
+            return messageId ? true : false;
+        } catch (error) {
+            throw new NotFoundException();
+        }
+    }
+
+    //Recover Multi Messages
+    async recoverBulk(deleted: DeleteMany): Promise<Messager[]> {
+        try {
+            const messageId = await this.repositoryMessager.find(
+                {
+                    where: {id: In(deleted.ids)},
+                    withDeleted: true,
+                });
+            await this.repositoryMessager.recover(messageId);
+            return messageId;
+        } catch (error) {
+            throw new NotFoundException();
+        }
+    }
+
+    //Recover One Message
+    async recover(id: string): Promise<Messager> {
+        try {
+            const messageId = await this.repositoryMessager.findOne({
+                where: { id },
+                withDeleted: true,
+            });
+            await this.repositoryMessager.recover(messageId);
+            return messageId;
+        } catch (error) {
+            throw new NotFoundException();
+        }
+    }
 }
